f32e1ad2487ae125fb67d57f1780adb4f4538868
Aditya Maheshwari <amaheshwari819@gmail.com>
2026-01-03 09:28:40 +0530

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Devtinder-app/',
+  base: '/',  // ✅ CHANGED: Set to root for Vercel
   plugins: [react() , tailwindcss()],
 })
